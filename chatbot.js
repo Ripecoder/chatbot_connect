9adeef5c9f37b6173bb517a825810144edf2f1f8
Ripecoder <n21816012@gmail.com>
2026-06-01 13:36:22 +0530

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -38,7 +38,7 @@
       .chatbot-btn {
         position: fixed;
         bottom: 28px;
-        right: 60px;
+        right: 70px;
         width: 56px;
         height: 56px;
         border-radius: 50%;
